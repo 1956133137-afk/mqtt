aa7182ded20d4ba92386d4f4a1d57369e99ce85f
何威 <1215592333@qq.com>
2024-09-06 14:09:55 +0800

    [优化] 刷脸界面跳转的问题

--- app/src/main/java/com/yannuo/dgcanteen/activitys/fragment/FacFragment.kt
+++ app/src/main/java/com/yannuo/dgcanteen/activitys/fragment/FacFragment.kt
@@ -1,12 +1,29 @@
 package com.yannuo.dgcanteen.activitys.fragment
 
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.ccb.smartcanteen.ZHSTFacePayService
+import com.google.gson.Gson
+import com.yannuo.dgcanteen.R
+import com.yannuo.dgcanteen.activitys.HostActivity
+import com.yannuo.dgcanteen.activitys.viewModel.ProductsVM
 import com.yannuo.dgcanteen.common.CameraAIDL
+import com.yannuo.dgcanteen.common.MyApplication
 import com.yannuo.dgcanteen.databinding.FragmentFacBinding
 import com.yannuo.dgcanteen.interfaces.CallbackListener
 import com.yannuo.dgcanteen.interfaces.IProductsVM
@@ -16,44 +33,51 @@ import com.yannuo.dgcanteen.model.SimpleForUI
 import com.yannuo.dgcanteen.util.CommonAndDpToPxUtil
 import com.yannuo.dgcanteen.util.Constant
 import com.yannuo.dgcanteen.util.LogUtil
+import com.yannuo.dgcanteen.util.TimeUtil
 import kotlinx.coroutines.*
 
-class FacFragment : Fragment(), CallbackListener {
-    private lateinit var binding: FragmentFacBinding
-    private val TAG = javaClass.simpleName
+class FacFragment : BaseFragment<FragmentFacBinding>(), IProductsVM {
+    private val productsVM by lazy {
+        ViewModelProvider(requireActivity())[ProductsVM::class.java]
+    }
+    val handler = Handler(MyApplication.applicationContext.mainLooper)
+    private var mFacePayService: ZHSTFacePayService? = null
 
-    private lateinit var mScope: CoroutineScope
-    private var resume = false
+    private val mServiceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            LogUtil.d(TAG, "onServiceConnected")
+            mFacePayService = ZHSTFacePayService.Stub.asInterface(service)
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+        override fun onServiceDisconnected(name: ComponentName) {
+            LogUtil.d(TAG, " onServiceDisconnected")
+        }
+    }
+
+    override fun bindLayout(inflater: LayoutInflater, container: ViewGroup?) {
         binding = FragmentFacBinding.inflate(inflater, container, false)
         initObject()
         initEvent()
-        mScope = CoroutineScope(Dispatchers.IO)
-        return binding.root
     }
 
-
-    override fun onResume() {
-        super.onResume()
-        resume = true
-        LogUtil.i(TAG, "resume...")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        LogUtil.i(TAG, "onStop...")
-        resume = false
+    private fun loadData() {
+        val lIntent = Intent()
+        lIntent.action = "com.ccb.smartcanteen.FacePayService"
+        lIntent.setPackage("com.ccb.smartcanteen")
+        requireContext().bindService(lIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
     }
 
 
     private fun initObject() {
+        loadData()
+        productsVM.listener = this
+        var payMoney = 0.0f
         arguments?.getParcelable<OrderPayInfo>(Constant.PAY_DATE)?.also {
             binding.payTotalMoney.text = "￥${it.payment}"
-            CameraAIDL.startCamera(it.payment, this@FacFragment)
+            payMoney = it.payment
+        }
+        handler.post {
+            productsVM.startPayWithFace(mFacePayService, payMoney)
         }
 
     }
@@ -64,45 +88,35 @@ class FacFragment : Fragment(), CallbackListener {
             requireActivity().finish()
         }
     }
-
-
-    override fun onOtherListener(event: Int, any: Any?) {
-        val payForUI = any as PayForUI
-        val bean = SimpleForUI().apply {
-            custName = payForUI.username
-            payment = payForUI.payment.toFloat() ?: 0f
-            accNo = payForUI.accNo
-            timestamp = payForUI.payTime
-            tranId = payForUI.traceId
-            orderId = payForUI.orderId
-            errorMsg = payForUI.errMsg
-            acc_bal = payForUI.accBal
-        }
-        mScope.launch {
-            try {
-                repeat(150) {
-                    if (isStateSaved && !resume) {
-                        delay(20)
-                        return@repeat
-                    }
-                    withContext(Dispatchers.Main) {
-                        if (payForUI.result == "Y") {
-                            CommonAndDpToPxUtil.speakWork("支付成功")
-                            val action = FacFragmentDirections.actionScanToSuccess(bean)
-                            findNavController().navigate(action)
-                        } else {
-                            val action = FacFragmentDirections.actionScanToFail(bean)
-
-                            CommonAndDpToPxUtil.speakWork("支付失败")
-                            findNavController().navigate(action)
-                        }
-                        LogUtil.d(TAG, "wait isStateSaved")
-                        cancel()
-                    }
-                }
-            } catch (e: Exception) {
-                LogUtil.e(TAG, "${e.cause} ${e.message}")
+    override fun onFacePayResult(payForUI: PayForUI) {
+        LogUtil.d(TAG, "人脸支付结束，准备跳转结果展示~")
+        handler.postDelayed({
+            val bean = SimpleForUI().apply {
+                way = payForUI.payType
+                timestamp = TimeUtil.timeFormat("yyyy-MM-dd HH:mm:ss", System.currentTimeMillis())
             }
-        }
+            LogUtil.d(TAG, "onFacePayResult: ${Gson().toJson(bean)}")
+
+            if (payForUI.result == "Y") {
+                bean.apply {
+                    payment = payForUI.payment.toFloat()
+                    custName = payForUI.username
+                    accNo = payForUI.accNo
+                    acc_bal = payForUI.accBal
+                    orderId = payForUI.orderId
+                }
+                CommonAndDpToPxUtil.speakWork("支付成功")
+                val action = FacFragmentDirections.actionScanToSuccess(bean)
+                findNavController().navigate(action)
+            } else if (payForUI.result == "N") {
+                bean.apply {
+                    errorMsg = payForUI.errMsg
+                }
+                CommonAndDpToPxUtil.speakWork("支付失败")
+                val action = FacFragmentDirections.actionScanToFail(bean)
+                findNavController().navigate(action)
+            }
+        }, 300)
     }
+
 }
